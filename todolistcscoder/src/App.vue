0020de5f966adff57029e4395ea8655eecb94c28
Caio Santana <brnoliveirasantana@gmail.com>
2022-08-01 23:59:28 -0300

    modified:   todolistcscoder/package.json 	modified:   todolistcscoder/public/index.html 	modified:   todolistcscoder/src/App.vue 	deleted:    todolistcscoder/src/assets/logo.png 	deleted:    todolistcscoder/src/components/HelloWorld.vue 	new file:   todolistcscoder/src/interfaces/ITask.ts 	modified:   todolistcscoder/src/main.ts 	modified:   todolistcscoder/yarn.lock

--- todolistcscoder/src/App.vue
+++ todolistcscoder/src/App.vue
@@ -1,27 +1,59 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <main class="columns is-gapless is-multiline">
+    <div class="column is-one-quarter"></div>
+    <div class="column is-three-quarter content">
+      <div class="list"></div>
+    </div>
+  </main>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import { defineComponent } from "vue";
+import ITask from "./interfaces/ITask";
 
 export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: "App",
+
+  components: {},
+
+  data() {
+    return {
+      tasks: [] as ITask[],
+      activeDarkMode: false,
+    };
+  },
+
+  computed: {
+    listThisEmpty(): boolean {
+      return this.tasks.length === 0;
+    },
+  },
+
+  methods: {
+    saveTask(task: ITask) {
+      this.tasks.push(task);
+    },
+    swapTheme(activeDarkMode: boolean) {
+      this.activeDarkMode = activeDarkMode;
+    },
+  },
 });
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.list {
+  padding: 1.25rem;
+}
+main {
+  --bg-primary: #fff;
+  --primary-text: #000;
+}
+main.dark-mode {
+  --bg-primary: #2b2d42;
+  --primary-text: #ddd;
+}
+
+.content {
+  background-color: var(--bg-primary);
 }
 </style>
